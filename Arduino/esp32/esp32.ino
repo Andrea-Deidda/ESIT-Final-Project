b0e8ce3e21c7df55031ca6bae15192b75bf6f3fc
Alessio Piriottu <72929470+AlessioPiriottu@users.noreply.github.com>
2022-07-17 21:44:46 +0200

    aggiunta funzionalità wifi

--- Arduino/esp32/esp32.ino
+++ Arduino/esp32/esp32.ino
@@ -23,13 +23,16 @@ BLEScan* pBLEScan;
 float distance;
 String device_name;
 int count = 0;
+String nome_rete;
+String distanza_rete;
+String scelta = "ble";
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       device_name = advertisedDevice.getName().c_str();
       if (device_name == "")
         device_name = "Dispositivo sconosciuto";
-
+      //int8_t device_pow = advertisedDevice.getTXPower();
       Serial.print(F("Advertised Device: "));
       Serial.println(device_name);
       distance = pow(10, ((-72.0 - advertisedDevice.getRSSI()) / 20.0));
@@ -127,6 +130,20 @@ void publishMessage()
   doc["deviceId"] = timeStamp; 
   doc["device"] = device_name;
   doc["distance"] = distance;
+  doc["protocollo"] = "BLE";
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+void publishMessageWifi()
+{
+  StaticJsonDocument<200> doc;
+  doc["deviceId"] = timeStamp; 
+  doc["device"] = nome_rete;
+  doc["distance"] = distanza_rete;
+  doc["protocollo"] = "Wifi";
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
 
@@ -158,7 +175,7 @@ void setup() {
   
 }
 
-void loop() {
+void BLEScan(){
   
   BLEScanResults foundDevices = pBLEScan->start(scanTime, false); 
   publishMessage();
@@ -168,5 +185,49 @@ void loop() {
   client.loop();
   //pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
   delay(5000);
+  
+  }
+
+void WifiScan(){
+  
+  Serial.println("scan start");
+
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            nome_rete = WiFi.SSID(i);
+            distanza_rete = pow(10, ((-72.0 - WiFi.RSSI(i)) / 20.0));
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            delay(10);
+            publishMessageWifi();
+        }
+    }
+    Serial.println("");
+
+    // Wait a bit before scanning again
+    delay(5000);
+  
+  }
+
+void loop() {
+
+  if (scelta == "ble")
+    BLEScan();
+    
+  if (scelta == "wifi")
+    WifiScan();
   
 }
