dd6158482e874e507333565875989d4a9a0204fa
Alessio Piriottu <72929470+AlessioPiriottu@users.noreply.github.com>
2022-08-04 17:09:43 +0200

    soglie e modifiche varie

--- Arduino/esp32/esp32.ino
+++ Arduino/esp32/esp32.ino
@@ -10,7 +10,6 @@
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 
-
 // The MQTT topics that this device should publish/subscribe
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
@@ -18,16 +17,12 @@
 #define AWS_IOT_PUBLISH_SHADOW_TOPIC "$aws/things/esit-obj1/shadow/update"
 #define AWS_IOT_SUBSCRIBE_SHADOW_TOPIC "$aws/things/esit-obj1/shadow/update/delta"
 
-//#define AWS_IOT_PUBLISH_SHADOW_TOPIC "$aws/things/esit-obj1/shadow/get"
-//#define AWS_IOT_SUBSCRIBE_SHADOW_TOPIC "$aws/things/esit-obj1/shadow/get/accepted"
-
-
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
 
-String sndPayloadWIFI = "{\"state\": { \"reported\": { \"protocollo\": \"wifi\" } }}";
-String sndPayloadBLE = "{\"state\": { \"reported\": { \"protocollo\": \"ble\" } }}";
+String sndPayloadWIFI = "{\"state\": { \"reported\": { \"protocollo\": \"wifi-10-3-10-1-3-1\" } }}";
+String sndPayloadBLE = "{\"state\": { \"reported\": { \"protocollo\": \"ble-10-3-10-1-3-1\" } }}";
 
 int scanTime = 5; //In seconds
 BLEScan* pBLEScan;
@@ -35,12 +30,23 @@ float distance;
 String device_name;
 int count = 0;
 String nome_rete;
-String distanza_rete;
+float distanza_rete;
 String address_ble;
 String address_wifi;
+String dati = "";
 String scelta = "ble";
 String rcvdPayload;
 bool msgReceived = false;
+
+String inputSafeRiskThreshold;
+String inputLowRiskThresholdMin;
+String inputLowRiskThresholdMax;
+String inputMediumRiskThresholdMin;
+String inputMediumRiskThresholdMax;
+String inputHighRiskThreshold;
+String liv_rischio = "";
+
+String livelloRischio(float misura);
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -48,12 +54,13 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       address_ble = advertisedDevice.getAddress().toString().c_str();
       if (device_name == "")
         device_name = "Dispositivo sconosciuto";
-      //int8_t device_pow = advertisedDevice.getTXPower();
-      Serial.print(F("Advertised Device: "));
-      Serial.println(device_name);
+
       distance = pow(10, ((-72.0 - advertisedDevice.getRSSI()) / 20.0));
-      Serial.printf("Distance: %.2f m\n", distance);
-      Serial.println(address_ble);
+      liv_rischio = livelloRischio(distance);
+
+      Serial.print(F("Device: "));
+      Serial.print(device_name);
+      Serial.printf(" distanza: %.2f m: %s\n", distance, liv_rischio);
     }
 };
 
@@ -67,7 +74,7 @@ void connectAWS()
 
   Serial.println("Connecting to Wi-Fi");
 
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -90,7 +97,7 @@ void connectAWS()
     delay(100);
   }
 
-  if(!client.connected()){
+  if (!client.connected()) {
     Serial.println("AWS IoT Timeout!");
     return;
   }
@@ -103,38 +110,38 @@ void connectAWS()
 }
 
 //funzione che restituisce un timestamp univoco per creare un ID per il messaggio MQTT
-String printLocalTime(){
+String printLocalTime() {
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return "Failed";
   }
 
   char timeHour[3];
-  strftime(timeHour,3, "%H", &timeinfo);
+  strftime(timeHour, 3, "%H", &timeinfo);
   String timeHourString(timeHour);
-  
+
   char timeYear[5];
-  strftime(timeYear,5, "%Y", &timeinfo);
+  strftime(timeYear, 5, "%Y", &timeinfo);
   String timeYearString(timeYear);
 
   char timeMonth[10];
-  strftime(timeMonth,10, "%B", &timeinfo);
+  strftime(timeMonth, 10, "%B", &timeinfo);
   String timeMonthString(timeMonth);
 
   char timeDay[3];
-  strftime(timeDay,3, "%d", &timeinfo);
+  strftime(timeDay, 3, "%d", &timeinfo);
   String timeDayString(timeDay);
 
-   char timeMinute[3];
-  strftime(timeMinute,3, "%M", &timeinfo);
+  char timeMinute[3];
+  strftime(timeMinute, 3, "%M", &timeinfo);
   String timeMinuteString(timeMinute);
 
   char timeSecond[3];
-  strftime(timeSecond,3, "%S", &timeinfo);
+  strftime(timeSecond, 3, "%S", &timeinfo);
   String timeMinuteSecond(timeSecond);
 
-  
+
   String timeStamp = timeYearString + "-" + timeMonthString + "-" + timeDayString + "-" + timeHourString + ":" + timeMinuteString + ":" + timeMinuteSecond;
 
   return timeStamp;
@@ -142,10 +149,10 @@ String printLocalTime(){
 
 
 void publishMessage()
-{ 
+{
   String timeStamp = printLocalTime();
   StaticJsonDocument<200> doc;
-  doc["deviceId"] = timeStamp; 
+  doc["deviceId"] = timeStamp;
   doc["device"] = device_name;
   doc["distance"] = distance;
   doc["protocollo"] = "BLE";
@@ -160,7 +167,7 @@ void publishMessageWifi()
 {
   String timeStamp = printLocalTime();
   StaticJsonDocument<200> doc;
-  doc["deviceId"] = timeStamp; 
+  doc["deviceId"] = timeStamp;
   doc["device"] = nome_rete;
   doc["distance"] = distanza_rete;
   doc["protocollo"] = "WIFI";
@@ -175,7 +182,7 @@ void publishMessageWifi()
 void messageHandler(String &topic, String &payload) {
   msgReceived = true;
   rcvdPayload = payload;
-  
+
   //StaticJsonDocument<200> doc;
   //deserializeJson(doc, payload);
   //const char* message = doc["state"]["protocollo"];
@@ -188,6 +195,13 @@ void setup() {
   Serial.begin(115200);
   connectAWS();
 
+  inputSafeRiskThreshold = "10";
+  inputLowRiskThresholdMin = "3";
+  inputLowRiskThresholdMax = "10";
+  inputMediumRiskThresholdMin = "1";
+  inputMediumRiskThresholdMax = "3";
+  inputHighRiskThreshold = "1";
+
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan(); //create new scan
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
@@ -199,95 +213,131 @@ void setup() {
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   printLocalTime();
 
-  //sprintf(sndPayloadBLE,"{\"state\": { \"reported\": { \"protocollo\": \"BLE\" } }}");
-  //sprintf(sndPayloadWIFI,"{\"state\": { \"reported\": { \"protocollo\": \"WIFI\" } }}");
-
   client.publish(AWS_IOT_PUBLISH_SHADOW_TOPIC, sndPayloadBLE);
-  
+
 }
 
-void BLEScan(){
-  
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false); 
+void BLEScan() {
+
+  Serial.println("BLE SCAN: ");
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
   publishMessage();
   Serial.print("Devices found: ");
   Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!"); 
+  Serial.println("Scan done!");
+  Serial.println("");
   client.loop();
   //pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
   delay(5000);
-  
-  }
 
-void WifiScan(){
-  
-  Serial.println("scan start");
+}
 
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            nome_rete = WiFi.SSID(i);
-            distanza_rete = pow(10, ((-72.0 - WiFi.RSSI(i)) / 20.0));
-            address_wifi = WiFi.macAddress();
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print(")");
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-            delay(10);
-            publishMessageWifi();
-        }
+void WifiScan() {
+
+  Serial.println("WIFI SCAN: ");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+    Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      nome_rete = WiFi.SSID(i);
+      distanza_rete = pow(10, ((-72.0 - WiFi.RSSI(i)) / 20.0));
+      address_wifi = WiFi.macAddress();
+
+      liv_rischio = livelloRischio(distanza_rete);
+
+      Serial.print(WiFi.SSID(i));
+      Serial.printf(" distanza: %.2f m: %s\n", distanza_rete, liv_rischio);
+      delay(10);
+      publishMessageWifi();
     }
-    Serial.println("");
-
-    // Wait a bit before scanning again
-    delay(5000);
-  
   }
+  Serial.println("");
+
+  // Wait a bit before scanning again
+  delay(5000);
+
+}
+
+String livelloRischio(float misura) {
+  String rischio = "";
+
+  //Serial.println(inputSafeRiskThreshold.toFloat());
+  //Serial.println(inputLowRiskThresholdMin.toFloat());
+  //Serial.println(inputLowRiskThresholdMax.toFloat());
+  //Serial.println(inputMediumRiskThresholdMin.toFloat());
+  //Serial.println(inputMediumRiskThresholdMax.toFloat());
+  //Serial.println(inputHighRiskThreshold.toFloat());
+
+  if (misura > inputSafeRiskThreshold.toFloat())
+    rischio = "SICURO";
+  else if (misura >= inputLowRiskThresholdMin.toFloat() && misura <= inputLowRiskThresholdMax.toFloat())
+    rischio = "RISCHIO BASSO";
+  else if (misura >= inputMediumRiskThresholdMin.toFloat() && misura < inputMediumRiskThresholdMax.toFloat())
+    rischio = "RISCHIO MEDIO";
+  else
+    rischio = "RISCHIO ALTO";
+
+  return rischio;
+}
 
 void loop() {
 
-  if(msgReceived){
+  if (msgReceived) {
     delay(100);
     msgReceived = false;
     Serial.print("Received Message:");
     Serial.println(rcvdPayload);
+    Serial.println("");
 
     StaticJsonDocument<200> doc;
     deserializeJson(doc, rcvdPayload);
     const char *message = doc["state"]["protocollo"];
-    scelta = String(message);
-    Serial.println(message);
-      
-      if(scelta == "ble"){
-         Serial.println("IF CONDITION");
-         Serial.println("Turning BLE");
-         client.publish(AWS_IOT_PUBLISH_SHADOW_TOPIC, sndPayloadBLE);
-        }
-        
-      if(scelta == "wifi") 
-        {
-         Serial.println("ELSE CONDITION");
-         Serial.println("Turning WIFI");
-         client.publish(AWS_IOT_PUBLISH_SHADOW_TOPIC, sndPayloadWIFI);
-       
+    dati = String(message);
+
+    int str_len = dati.length() + 1;
+    char char_array[str_len];
+    dati.toCharArray(char_array, str_len);
+
+    scelta = strtok(char_array, "-");
+    inputSafeRiskThreshold = strtok(NULL, "-");
+    inputLowRiskThresholdMin = strtok(NULL, "-");
+    inputLowRiskThresholdMax = strtok(NULL, "-");
+    inputMediumRiskThresholdMin = strtok(NULL, "-");
+    inputMediumRiskThresholdMax = strtok(NULL, "-");
+    inputHighRiskThreshold = strtok(NULL, "-");
+
+    String payloadBLE = "{\"state\": { \"reported\": { \"protocollo\": \"ble-" + inputSafeRiskThreshold + "-" + inputLowRiskThresholdMin + "-" + inputLowRiskThresholdMax + "-" + inputMediumRiskThresholdMin + "-" + inputMediumRiskThresholdMax + "-" + inputHighRiskThreshold +"\" } }}";
+    String payloadWIFI = "{\"state\": { \"reported\": { \"protocollo\": \"wifi-" + inputSafeRiskThreshold + "-" + inputLowRiskThresholdMin + "-" + inputLowRiskThresholdMax + "-" + inputMediumRiskThresholdMin + "-" + inputMediumRiskThresholdMax + "-" + inputHighRiskThreshold +"\" } }}";
+    Serial.println(payloadBLE);
+    Serial.println(payloadWIFI);
+    
+    if (scelta == "ble") {
+      Serial.println("Turning BLE");
+      client.publish(AWS_IOT_PUBLISH_SHADOW_TOPIC, payloadBLE);
+    }
+
+    if (scelta == "wifi")
+    {
+      Serial.println("Turning WIFI");
+      client.publish(AWS_IOT_PUBLISH_SHADOW_TOPIC, payloadWIFI);
+
     }
   }
 
   //Serial.println(msgReceived);
   if (scelta == "ble")
     BLEScan();
-  
-       
+
+
   if (scelta == "wifi")
     WifiScan();
 
--- Frontend/src/app/routes/manage/manage.component.ts
+++ Frontend/src/app/routes/manage/manage.component.ts
@@ -11,18 +11,24 @@ import { ShadowService } from '../../service/shadow/shadow.service';
 })
 export class ManageComponent implements OnInit {
 
-  constructor(private shadowService: ShadowService, private router: Router) { }
+  constructor(private shadowService: ShadowService, private router: Router) {
+    this.inputSafeRiskThreshold = 10
+    this.inputLowRiskThresholdMin = 3
+    this.inputLowRiskThresholdMax = 10
+    this.inputMediumRiskThresholdMin = 1
+    this.inputMediumRiskThresholdMax = 3
+    this.inputHighRiskThreshold = 1
+  }
 
-  
-  inputFirstThreshold!: number; //si deve fare per tutti e 6 
-  inputSecondThresholdMin!: number;
-  inputSecondThresholdMax!: number;
-  inputThirdThresholdMin!: number;
-  inputThirdThresholdMax!: number;
-  inputFourthThreshold!: number;
+  inputSafeRiskThreshold!: number;
+  inputLowRiskThresholdMin!: number;
+  inputLowRiskThresholdMax!: number;
+  inputMediumRiskThresholdMin!: number;
+  inputMediumRiskThresholdMax!: number;
+  inputHighRiskThreshold!: number;
 
   radioButton!: string;
-  
+
   status!: string;
 
   ngOnInit(): void {
@@ -30,22 +36,39 @@ export class ManageComponent implements OnInit {
   }
 
   onSubmit(){
-    if(this.inputFirstThreshold != undefined) 
-    {
-      console.log("sicuro > " + this.inputFirstThreshold);
-      console.log("rischio basso (< " + this.inputSecondThresholdMin + " && > " + this.inputSecondThresholdMax + ")" );
-      console.log("rischio medio (< " + this.inputThirdThresholdMin + " && > " + this.inputThirdThresholdMax + ")" );
-      console.log("alto < " + this.inputFourthThreshold);
-    }
 
-    if(this.radioButton != undefined)
-      console.log(this.radioButton);
-      this.shadowService.setProtocoll(this.radioButton).subscribe(response => {
-        console.log(response);
-      }), (err: any) => {
+    if( this.inputSafeRiskThreshold < this.inputLowRiskThresholdMax ||
+        this.inputLowRiskThresholdMax < this.inputLowRiskThresholdMin ||
+        this.inputLowRiskThresholdMin < this.inputMediumRiskThresholdMax ||
+        this.inputMediumRiskThresholdMax < this.inputMediumRiskThresholdMin ||
+        this.inputMediumRiskThresholdMin < this.inputHighRiskThreshold) {
+
+      window.alert("Invalid threshold values!");
+
+    } else {
+      if (this.radioButton == undefined) {
+        window.alert("Select a communication protocol!");
+
+      } else {
+
+        console.log(this.radioButton);
+        console.log("sicuro oltre " + this.inputSafeRiskThreshold + " metri");
+        console.log("rischio basso tra " + this.inputLowRiskThresholdMin + " e " + this.inputLowRiskThresholdMax + " metri");
+        console.log("rischio medio tra " + this.inputMediumRiskThresholdMin + " e " + this.inputMediumRiskThresholdMax + " metri");
+        console.log("rischio alto entro " + this.inputHighRiskThreshold + " metri");
+
+        this.shadowService.setProtocoll(this.radioButton + '-' + this.inputSafeRiskThreshold +
+          '-' + this.inputLowRiskThresholdMin + '-' + this.inputLowRiskThresholdMax + '-' +
+          this.inputMediumRiskThresholdMin + '-' + this.inputMediumRiskThresholdMax + '-' + this.inputHighRiskThreshold)
+          .subscribe(response => {
+            console.log(response);
+          }), (err: any) => {
           console.log(err);
+        }
+
+        window.alert("Options submitted!"); //messaggio che informa l'utente dell'avenuto cambiamento
+      }
     }
-    window.alert("Options submitted"); //messaggio che informa l'utente dell'avenuto cambiamento
   }
 
   reloadPage() {
@@ -55,7 +78,7 @@ export class ManageComponent implements OnInit {
   getStatus(){
     this.shadowService.getStatus().subscribe(( response : any) => {
       //console.log(response.state.reported.protocollo)
-      this.status = response.state.reported.protocollo;
+      this.status = response.state.reported.protocollo.split("-", 1);
       console.log(this.status);
       if (this.status == 'ble'){
         this.status = 'Bluetooth'
